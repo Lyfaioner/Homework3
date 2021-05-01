0ee5ca1a5cfaa6dec97eb54ea23554b4df0592b7
Sinan Türk <78880473+Lyfaioner@users.noreply.github.com>
2021-05-01 21:45:05 +0300

    Update InstructorService.java

--- Kodlamaio/src/InstructorService.java
+++ Kodlamaio/src/InstructorService.java
@@ -1,5 +1,5 @@
 
 public class InstructorService {
-public void Add(Instructor ınstructor) {System.out.println("Eğitmen eklendi")
-public void Del(Instructor ınstructor) {System.out.println("Eğitmen Silindi")
+public void Add(Instructor instructor) {System.out.println("Eğitmen eklendi")
+public void Del(Instructor instructor) {System.out.println("Eğitmen Silindi")
 }
